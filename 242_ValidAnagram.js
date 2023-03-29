51a18f0fe5d000ecf11f4cd0dc9748543771d9e5
Adam Polk <adampolk91@gmail.com>
2023-03-29 12:12:30 -0500

    updates some missing syntax

--- 242_ValidAnagram.js
+++ 242_ValidAnagram.js
@@ -8,6 +8,7 @@ var isAnagram = function(s, t) {
   // iterate through both strings, storing the values in an object and setting them equal to 1. If a repetition is found, increment that letter to allow for more uses. The second pass (stringTwo) will decrement those values. If a value is not found on second pass, they are not anagrams. If a numeric value is less than or greater than zero, they are not anagrams.
 
   const bank = {};
+
   for (let i = 0; i < s.length; i++) {
       let currentLetter = s[i]
       if (!bank[currentLetter]) {
@@ -19,7 +20,7 @@ var isAnagram = function(s, t) {
   for (let i = 0; i < t.length; i++) {
       let currentLetter = t[i];
       if (!bank[currentLetter]) {
-          return false
+          return false;
       } else {
           bank[currentLetter]--;
       }
@@ -27,7 +28,7 @@ var isAnagram = function(s, t) {
 
   for (letter in bank) {
       if (bank[letter] > 0 || bank[letter] < 0) {
-          return false
+          return false;
       }
   }
 
